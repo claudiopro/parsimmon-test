42a59de623f5a6e8a92a5626c9015ec0f6a5b429
claudiopro <claudio.procida@gmail.com>
2015-09-20 15:57:17 +0200

    Added a different grammar

--- test/test.js
+++ test/test.js
@@ -10,12 +10,16 @@ var lparen = lexeme(string('('));
 var rparen = lexeme(string(')'));
 
 var expr = lazy('an s-expression', function() { return form.or(atom) });
+var expr2 = lazy('an s-expression', function() { return form2.or(operator) });
 
 var number = lexeme(regex(/[0-9]+/).map(parseInt));
 var id = lexeme(regex(/[a-z_]\w*/i));
+var op = lexeme(regex(/[\+\*\/\-]\w*/i));
 
 var atom = number.or(id);
+var operator = number.or(op);
 var form = lparen.then(expr.many()).skip(rparen);
+var form2 = lparen.then(expr2.many()).skip(rparen);
 
 describe('the parser', function() {
   it('parses a scalar value', function() {
@@ -32,4 +36,16 @@ describe('the parser', function() {
     expect(value[1][2][2]).toEqual(4);
     expect(value[2]).toEqual(['add', 7, 8]);
   });
+
+  it('parses a symbolic expression', function() {
+    var value = expr2.parse('(+ (* 10 (+ 3 4)) (+ 7 8))').value;
+    expect(value[0]).toEqual('+');
+    expect(value[1][0]).toEqual('*');
+    expect(value[1][1]).toEqual(10);
+    expect(value[1][2][0]).toEqual('+');
+    expect(value[1][2][1]).toEqual(3);
+    expect(value[1][2][2]).toEqual(4);
+    expect(value[2]).toEqual(['+', 7, 8]);
+  });
+
 });
